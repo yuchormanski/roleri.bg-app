981022d65597007038fb7a49e7f4555587ad5118
Todor Yadkov <toni_12@abv.bg>
2024-02-13 16:50:43 +0200

    Add server endpoints

--- client/src/services/environment.js
+++ client/src/services/environment.js
@@ -1,8 +1,21 @@
 const BASE_URL = "http://localhost:3000/";
 
+const SERVER_ENDPOINTS = {
+  REGISTER: 'users/register',
+  LOGIN: 'users/login',
+  LOGOUT: 'users/logout',
+  FORGOT_PASSWORD: 'users/forgot-password',
+  RESET_PASSWORD: 'users/reset-password',
+};
+
 const EMAIL_REGEX =
   /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
 
 const PASS_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/;
 
-export { BASE_URL, EMAIL_REGEX, PASS_REGEX };
+export {
+  BASE_URL,
+  EMAIL_REGEX,
+  PASS_REGEX,
+  SERVER_ENDPOINTS,
+};
