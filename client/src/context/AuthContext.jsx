69855a242050ddc2e06573cf91ee0c53d72d5cd6
Todor Yadkov <toni_12@abv.bg>
2024-02-15 22:23:37 +0200

    Create AuthContext

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,66 @@
+import { createContext, useContext } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+
+import { USER_LOCAL_STORAGE_KEY } from "../services/environment.js";
+
+const AuthContext = createContext();
+
+function AutContextProvider({ children }) {
+    const queryClient = useQueryClient();
+
+    const addUserHandler = (data) => {
+        localStorage.setItem(USER_LOCAL_STORAGE_KEY, JSON.stringify(data));
+    };
+
+    const getUserHandler = () => {
+        try {
+            const localStorageData = JSON.parse(localStorage.getItem(USER_LOCAL_STORAGE_KEY));
+            return localStorageData ? localStorageData : null;
+
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
+    };
+
+    const removeUserHandler = () => {
+        localStorage.removeItem(USER_LOCAL_STORAGE_KEY);
+    };
+
+    const checkIsUserLoggedIn = () => {
+        // Get user data from React Query
+        const userDataFromQuery = queryClient.getQueryData(["user"]);
+        if (userDataFromQuery) {
+            return true;
+        } else {
+            // Get user data from local storage
+            const userDataFromLocalStorage = getUserHandler();
+            return !!userDataFromLocalStorage;
+        }
+    };
+
+    const values = {
+        addUserHandler,
+        getUserHandler,
+        removeUserHandler,
+        checkIsUserLoggedIn,
+    };
+
+    return (
+        <AuthContext.Provider value={values}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+function useAuthContext() {
+    const context = useContext(AuthContext);
+
+    if (context === undefined) {
+        throw new Error("AuthContext is used outside the context provider");
+    }
+
+    return context;
+}
+
+export { AutContextProvider, useAuthContext };
