501e05d336abafb26b904587039fa7dbad39b8a8
Todor Yadkov <toni_12@abv.bg>
2024-02-18 21:43:15 +0200

    Add configuration to start scraping on interval time and remove intervalId on stop server

--- server/config/expressConfig.js
+++ server/config/expressConfig.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import jwtMiddleware from '../middlewares/jwtMiddleware.js';
 import checkBlackListToken from '../middlewares/checkBlackListToken.js';
+import { scrapingNewsOnInterval, stopScrapingNewsOnInterval } from '../util/scrapingNewsOnInterval.js';
 
 const expressConfig = async (app, config) => {
 
@@ -18,6 +19,14 @@ const expressConfig = async (app, config) => {
     app.use(await checkBlackListToken());
     app.use(jwtMiddleware());
 
+    // Run scraping news on interval time (default hourly)
+    scrapingNewsOnInterval();
+
+    // Handle shutdown to stop the scrapingNewsOnInterval
+    process.on('SIGINT', () => {
+        stopScrapingNewsOnInterval();
+        process.exit();
+    });
 };
 
 export default expressConfig;
