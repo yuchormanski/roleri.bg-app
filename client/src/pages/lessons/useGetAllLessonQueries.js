1b3270bddccf19b14c8949b1f51f3f0dfa2da0e0
Todor Yadkov <toni_12@abv.bg>
2024-02-18 21:40:44 +0200

    Renaming and destructured to return only needed properties

--- client/src/pages/lessons/useGetAllLessonQueries.js
+++ client/src/pages/lessons/useGetAllLessonQueries.js
@@ -1,20 +1,17 @@
-import toast from "react-hot-toast";
 import { useQuery } from "@tanstack/react-query";
 
 import { get } from "../../api/api.js";
 import { SERVER_ENDPOINTS } from "../../services/environment.js";
 
-function useLessonQueries() {
-  const getAllLessonsQuery = useQuery({
+function useGetAllLessonQueries() {
+  const { isLoading, isError, error, data } = useQuery({
     queryKey: ["lessons"],
     queryFn: () => get(SERVER_ENDPOINTS.GET_ALL_LESSONS),
     // TODO - change this property with more accurate method to resolve situation with infinity refetch data on error
     initialData: [],
   });
 
-  return {
-    getAllLessonsQuery,
-  };
+  return { isLoading, isError, error, data };
 }
 
-export { useLessonQueries };
+export { useGetAllLessonQueries };
