8a35e00ba6bfaddd81e1055788e642df8801b929
Todor Yadkov <toni_12@abv.bg>
2024-02-18 21:43:32 +0200

    Refactoring to return news

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 
+import { endpoints } from '../environments/endPoints.js';
+import { getAllNews } from '../services/homeService.js';
+
 const homeController = Router();
 
-homeController.get('/', (req, res, next) => {
+homeController.get(endpoints.news, async (req, res, next) => {
     try {
-        const user = req.user;
-        console.log('TEST ENDPOINT')
-        console.log(user);
+        const allNews = await getAllNews();
 
-        res.status(200).json('TEST ENDPOINT');
+        res.status(200).json(allNews);
     } catch (error) {
         next(error);
     }
