09dc6f4ede0fd129e68214799d73731c5f7e081c
Todor Yadkov <toni_12@abv.bg>
2024-02-18 21:38:58 +0200

    Use scraped data from server to show news and example card for news

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,69 +1,49 @@
+import toast from "react-hot-toast";
+
 import styles from "./Home.module.css";
+import { FaRegFolderOpen } from "react-icons/fa";
+
+import { useLanguage } from "../../context/Language.jsx";
+import { useGetAllNewsQueries } from "./useGetAllNewsQueries.js";
+import Spinner from "../../ui/elements/spinner/Spinner.jsx";
 
 function Home() {
-  return (
-    <>
-      <h1 className={styles.heading}>Home</h1>
-      <section className={styles.news}>
-        <div>
-          <li className="item-post clearfix">
-            <a
-              className="recentImgRoll"
-              href="https://roleri.bg/lessons-in-hall-23-24/"
-              title="Уроци в зала"
-            >
-              <img
-                width="50"
-                height="50"
-                src="https://roleri.bg/wp-content/uploads/2023/10/lessons_hall_23-24-300x300.png"
-                className="attachment-kiddie-square-thumb size-kiddie-square-thumb wp-post-image"
-                alt=""
-                decoding="async"
-              />
-            </a>
-            <div className="recentLabelRoll">
-              <h6>
-                <a
-                  href="https://roleri.bg/lessons-in-hall-23-24/"
-                  title="Уроци в зала"
-                >
-                  Уроци в зала{" "}
-                </a>
-              </h6>
-              <span className="ztl-recent-post-date">
-                <i className="flaticon-calendar64"></i>
-                <a
-                  href="https://roleri.bg/lessons-in-hall-23-24/"
-                  title="Уроци в зала"
-                >
-                  31/10/2023{" "}
-                </a>
-              </span>
-            </div>
-            <p className="recentPostRoll">
-              &nbsp; Уроците в парка приключиха, но от първата неделя на ноември
-              (05.11) продължаваме в зала. А тя се намира на бул. Черни връх 47,
-              ет. 2 (бивш завод Витоша) входа е до Lidl. Заниманията отново ще
-              са в неделя от 10:00 часа и от 11:00 часа. През формата за
-              записване може да заявите участие.
-            </p>
-            <a
-              className="ztl-button recentBtnRoll"
-              href="https://roleri.bg/lessons-in-hall-23-24/"
-            >
-              Още
-            </a>
-          </li>
-        </div>
-        <div>
-          Far far away, behind the word mountains, far from the countries
-          Vokalia and Consonantia, there live the blind texts. Separated they
-          live in Bookmarksgrove right at the coast of the Semantics, a large
-          language ocean.{" "}
-        </div>
-      </section>
-    </>
-  );
+    const { lang } = useLanguage();
+    const { isLoading, isError, error, data: news } = useGetAllNewsQueries();
+
+    if (isError) {
+        toast.error(error.message);
+    }
+
+    return (
+        <>
+            <h1 className={styles.heading}>{lang.home}</h1>
+            {isLoading
+                ? (<Spinner />
+                ) : (
+                    <section className={styles.news}>
+                        {news.map(n => (
+                            <article key={n._id} className={styles.article}>
+                                <div className={styles.articleHeader}>
+                                    <div className={styles.imageWrapper}>
+                                        <img src={n.imageUrl} alt={n.title} />
+                                    </div>
+                                    <h2 className={styles.articleTitle}>{n.title}
+                                        <span><FaRegFolderOpen /> {n.date}</span>
+                                    </h2>
+                                </div>
+                                <div className={styles.articleBody}>
+                                    <p>{n.content}</p>
+                                    <a href={n.newsUrl} target="_blank" rel="noopener noreferrer" className={styles.linkPrimary}>
+                                        {lang.details}
+                                    </a>
+                                </div>
+                            </article>
+                        ))}
+                    </section>
+                )}
+        </>
+    );
 }
 
 export default Home;
