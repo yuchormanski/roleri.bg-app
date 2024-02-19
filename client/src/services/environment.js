e9ef9b715554249547ab6a486593e38e08e210e4
yuchormanski <yuchormanski@gmail.com>
2024-02-19 23:15:26 +0200

    added vercel url

--- client/src/services/environment.js
+++ client/src/services/environment.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:3000/";
+// const BASE_URL = "http://localhost:3000/";
+const BASE_URL = "https://roleri-bg-app.vercel.app/";
 
 const SERVER_ENDPOINTS = {
   REGISTER: "users/register",
