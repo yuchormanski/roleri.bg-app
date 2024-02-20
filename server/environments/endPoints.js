edc272be149796e870fcc9c5a4a4cbdb7c920b01
Todor Yadkov <toni_12@abv.bg>
2024-02-20 16:00:42 +0200

    Add new end point

--- server/environments/endPoints.js
+++ server/environments/endPoints.js
@@ -10,9 +10,10 @@ export const endpoints = {
   register: "/register",
   login: "/login",
   logout: "/logout",
+  get_user: "/get-user",
+  get_skater: "/get-skater",
+  update_user: "/update-user",
   forgot_password: "/forgot-password",
   reset_password: "/reset-password",
   news: "/news",
-  get_user: "/get-user",
-  get_skater: "/get-skater",
 };
