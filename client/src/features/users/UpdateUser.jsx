b2f28a24438cc417e7b63aecc88981f94a01b79c
Todor Yadkov <toni_12@abv.bg>
2024-02-20 15:58:20 +0200

    Use authContext to get initial data from current user

--- client/src/features/users/UpdateUser.jsx
+++ client/src/features/users/UpdateUser.jsx
@@ -1,35 +1,37 @@
 import styles from "./UpdateUser.module.css";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { usePath } from "../../context/PathContext.jsx";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
-import Input from "../../ui/elements/Input.jsx";
 import {
   EMAIL_REGEX,
   PHONE_REGEX,
-  SERVER_ENDPOINTS,
 } from "../../services/environment.js";
 import { useLanguage } from "../../context/Language.jsx";
 import { toast } from "react-hot-toast";
-import { get } from "../../api/api.js";
+import { useAuthContext } from "../../context/AuthContext.jsx";
+import { useUpdateUserQuery } from "./useUpdateUserQuery.js";
+import Spinner from "../../ui/elements/spinner/Spinner.jsx";
 
 function UpdateUser() {
   const { path, newPath } = usePath();
 
-  const queryClient = useQueryClient();
-  const data = queryClient.getQueryData(["user"]);
+  const { updateUserMutation } = useUpdateUserQuery();
+  const { getUserHandler } = useAuthContext();
+  const data = getUserHandler();
 
   const { lang } = useLanguage();
 
   useEffect(() => newPath("edit"), [newPath]);
 
-  const { register, handleSubmit, reset, getValues, formState } = useForm({
+  const { register, handleSubmit, reset } = useForm({
     defaultValues: data,
   });
 
-  function onFormSubmit(data) {
-    console.log(data);
+  async function onFormSubmit(data) {
+    const { _id, ...dataFromServer } = data;
+    await updateUserMutation.mutateAsync(dataFromServer);
+    reset();
   }
 
   function onErrorForm(error) {
@@ -39,86 +41,91 @@ function UpdateUser() {
     });
   }
 
-  const isLoading = false;
+  const isLoading = updateUserMutation.isPending;
   return (
     <div className={styles.container}>
       <h3 className={styles.heading}>{lang.update}</h3>
-      <form
-        className={styles.form}
-        onSubmit={handleSubmit(onFormSubmit, onErrorForm)}
-      >
-        <input
-          disabled={isLoading}
-          className={styles.input}
-          type="text"
-          id="firstName"
-          {...register("firstName", {
-            required: "First name is required",
-            maxLength: {
-              value: 30,
-              message: "First name can't be more than 30 characters long!",
-            },
-          })}
-          placeholder={"First name"}
-          autoComplete="given-name"
-        />
-        <input
-          disabled={isLoading}
-          className={styles.input}
-          type="text"
-          id="lastName"
-          {...register("lastName", {
-            required: "Last name is required",
-            maxLength: {
-              value: 30,
-              message: "Last name can't be more than 30 characters long!",
-            },
-          })}
-          placeholder={"Last name"}
-          autoComplete="family-name"
-        />
-        <input
-          disabled={isLoading}
-          className={styles.input}
-          type="email"
-          id="email"
-          {...register("email", {
-            required: "Email is required",
-            pattern: {
-              value: EMAIL_REGEX,
-              message: "Invalid email address",
-            },
-            maxLength: {
-              value: 30,
-              message: "Email can't be more than 30 characters long!",
-            },
-          })}
-          placeholder={"Email"}
-          autoComplete="email"
-        />
-        <input
-          disabled={isLoading}
-          className={styles.input}
-          type="tel"
-          id="phone"
-          {...register("phone", {
-            required: "Phone number is required",
-            pattern: {
-              value: PHONE_REGEX,
-              message: "Invalid phone number address",
-            },
-            maxLength: {
-              value: 20,
-              message: "Phone can't be more than 20 characters long!",
-            },
-          })}
-          placeholder={"Phone number"}
-          autoComplete="tel"
-        />
-        <button className={styles.updateBtn} disabled={isLoading}>
-          {lang.send}
-        </button>
-      </form>
+      {updateUserMutation.isPending
+        ? (
+          <Spinner />
+        ) : (
+          <form
+            className={styles.form}
+            onSubmit={handleSubmit(onFormSubmit, onErrorForm)}
+          >
+            <input
+              disabled={isLoading}
+              className={styles.input}
+              type="text"
+              id="firstName"
+              {...register("firstName", {
+                required: "First name is required",
+                maxLength: {
+                  value: 30,
+                  message: "First name can't be more than 30 characters long!",
+                },
+              })}
+              placeholder={"First name"}
+              autoComplete="given-name"
+            />
+            <input
+              disabled={isLoading}
+              className={styles.input}
+              type="text"
+              id="lastName"
+              {...register("lastName", {
+                required: "Last name is required",
+                maxLength: {
+                  value: 30,
+                  message: "Last name can't be more than 30 characters long!",
+                },
+              })}
+              placeholder={"Last name"}
+              autoComplete="family-name"
+            />
+            <input
+              disabled={isLoading}
+              className={styles.input}
+              type="email"
+              id="email"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: EMAIL_REGEX,
+                  message: "Invalid email address",
+                },
+                maxLength: {
+                  value: 30,
+                  message: "Email can't be more than 30 characters long!",
+                },
+              })}
+              placeholder={"Email"}
+              autoComplete="email"
+            />
+            <input
+              disabled={isLoading}
+              className={styles.input}
+              type="tel"
+              id="phone"
+              {...register("phone", {
+                required: "Phone number is required",
+                pattern: {
+                  value: PHONE_REGEX,
+                  message: "Invalid phone number",
+                },
+                maxLength: {
+                  value: 20,
+                  message: "Phone can't be more than 20 characters long!",
+                },
+              })}
+              placeholder={"Phone number"}
+              autoComplete="tel"
+            />
+            <button className={styles.updateBtn} disabled={isLoading}>
+              {lang.send}
+            </button>
+          </form>
+        )}
     </div>
   );
 }
