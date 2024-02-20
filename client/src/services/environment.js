1bba131808df966ba25e1bcd326672ff0e1bd565
Todor Yadkov <toni_12@abv.bg>
2024-02-20 16:00:04 +0200

    Use environment to make request to the back-end

--- client/src/services/environment.js
+++ client/src/services/environment.js
@@ -1,15 +1,16 @@
-// const BASE_URL = "http://localhost:3000/";
-const BASE_URL = "https://roleri-bg-app.vercel.app/";
+const reactEnvironment = import.meta.env.VITE_REACT_ENV;
+const BASE_URL = reactEnvironment === 'production' ? "https://roleri-bg-app.vercel.app/" : "http://localhost:3000/";
 
 const SERVER_ENDPOINTS = {
   REGISTER: "users/register",
   LOGIN: "users/login",
   LOGOUT: "users/logout",
+  GET_USER: "users/get-user",
+  UPDATE_USER: "users/update-user",
   FORGOT_PASSWORD: "users/forgot-password",
   RESET_PASSWORD: "users/reset-password",
   GET_ALL_LESSONS: "lessons",
   GET_ALL_NEWS: "news",
-  GET_USER: "users/get-user",
 };
 
 const EMAIL_REGEX =
@@ -25,8 +26,7 @@ const path = {
   logout: "users/logout",
 };
 
-const USER_LOCAL_STORAGE_KEY =
-  "08180818a772caa4b9a0787a7e065311e1e83e676b5ce1e83e676b5c";
+const USER_LOCAL_STORAGE_KEY = "08180818a772caa4b9a0787a7e065311e1e83e676b5ce1e83e676b5c";
 
 export {
   BASE_URL,
