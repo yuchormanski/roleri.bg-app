10852b89dc7d926561973e5edb699ba02a4c03f1
Todor Yadkov <toni_12@abv.bg>
2024-02-20 15:58:47 +0200

    Get data from current user from useAuthContext

--- client/src/features/users/UserInfo.jsx
+++ client/src/features/users/UserInfo.jsx
@@ -1,19 +1,20 @@
 import { useEffect } from "react";
 import { usePath } from "../../context/PathContext.jsx";
 import styles from "./UserInfo.module.css";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQueryClient } from "@tanstack/react-query";
 import { useLanguage } from "../../context/Language.jsx";
-import { SERVER_ENDPOINTS } from "../../services/environment.js";
-import { get } from "../../api/api.js";
 import Spinner from "../../ui/elements/spinner/Spinner.jsx";
+import { useAuthContext } from "../../context/AuthContext.jsx";
 
 function UserInfo() {
   const { path, newPath } = usePath();
   const { lang } = useLanguage();
   let isWorking = true;
 
-  const queryClient = useQueryClient();
-  const data = queryClient.getQueryData(["user"]);
+  // const queryClient = useQueryClient();
+
+  const { getUserHandler } = useAuthContext();
+  const data = getUserHandler();
   if (data) {
     isWorking = false;
   }
@@ -33,19 +34,21 @@ function UserInfo() {
   useEffect(() => newPath("profile"), [newPath]);
   return (
     <>
-      {isWorking ? (
-        <Spinner />
-      ) : (
-        <div className={styles.container}>
-          <h3
-            className={styles.heading}
-          >{`${data?.firstName} ${data?.lastName}'s ${lang.dashboard}`}</h3>
-          {lessons && <h3>You have no active lessons.</h3>}
-          <p>
-            Трябва да се зарежда информация дали има предстоящи записани уроци.
-          </p>
-        </div>
-      )}
+      {isWorking
+        ? (
+          <Spinner />
+        ) : (
+          <div className={styles.container}>
+            <h3 className={styles.heading}>
+              {`${data?.firstName} ${data?.lastName}'s ${lang.dashboard}`}
+            </h3>
+            {lessons && <h3>You have no active lessons.</h3>}
+            <p>
+              Трябва да се зарежда информация дали има предстоящи записани уроци.
+            </p>
+          </div >
+        )
+      }
     </>
   );
 }
