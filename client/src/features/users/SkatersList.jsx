c6aa5ddaa69eb5b27643b10b39485b53e8439dbf
Todor Yadkov <toni_12@abv.bg>
2024-02-21 19:14:30 +0200

    Refactoring to get dynamically data

--- client/src/features/users/SkatersList.jsx
+++ client/src/features/users/SkatersList.jsx
@@ -1,98 +1,97 @@
 import styles from "./SkatersList.module.css";
 
-import { useLanguage } from "../../context/Language.jsx";
-
-import { useQuery } from "@tanstack/react-query";
-
+import toast from "react-hot-toast";
+import { TbUserEdit } from "react-icons/tb";
 import {
   LiaUserEditSolid,
   LiaTrashAlt,
   LiaGrin,
   LiaIdCard,
 } from "react-icons/lia";
-import { TbUserEdit } from "react-icons/tb";
 
-// REMOVE THIS
-function test() {
-  return [
-    {
-      firstName: "Stamat",
-      lastName: "Georgiev",
-      gender: "male",
-      age: 6,
-      _id: 1,
-      level: "advanced",
-    },
-    {
-      firstName: "Dimitrinka",
-      lastName: "Georgieva",
-      gender: "femaile",
-      age: 4,
-      _id: 23,
-      level: "basic",
-    },
-  ];
-}
-// END REMOVE
+import { useLanguage } from "../../context/Language.jsx";
+import { useToggleModal } from "../../hooks/useToggleModal.js";
+import { useGetSkatersQuery } from "./useGetSkatersQuery.js";
+import AddSkater from "./AddSkater.jsx";
+import Spinner from "../../ui/elements/spinner/Spinner.jsx";
+import { useState } from "react";
+import EditSkater from "./EditSkater.jsx";
+import DeleteSkater from "./DeleteSkater.jsx";
 
 function SkatersList() {
+  const [selectedSkaterData, setSelectedSkaterData] = useState({});
+
+  const [isShownAddSkaterModal, toggleAddSkaterModalHandler] = useToggleModal();
+  const [isShownEditSkaterModal, toggleEditSkaterModalHandler] = useToggleModal();
+  const [isShownDeleteSkaterModal, toggleDeleteSkaterModalHandler] = useToggleModal();
+
   const { lang } = useLanguage();
-  // TODO: Create HOOK
-  const {
-    isLoading,
-    isError,
-    error,
-    data: skaters,
-  } = useQuery({
-    queryKey: ["skaters"],
-    queryFn: test,
-    // initialData: [],
-  });
+  const { isLoading, isError, error, data: skaters } = useGetSkatersQuery();
+
+  if (isError) {
+    toast.error(error.message);
+  }
+
+  function onEditSkater(skaterData) {
+    setSelectedSkaterData(skaterData);
+    toggleEditSkaterModalHandler();
+  }
+
+  function onDeleteSkater(skaterData) {
+    setSelectedSkaterData(skaterData);
+    toggleDeleteSkaterModalHandler();
+  }
 
   return (
     <div className={styles.container}>
       <h3 className={styles.heading}>{lang.skaters}</h3>
-
-      <div className={styles.skatersContainer}>
-        {skaters &&
-          skaters.map((skater) => (
-            <figure className={styles.figure} key={skater._id}>
-              <header className={styles.header}>
-                <h3
-                  className={styles.figureHeading}
-                >{`${skater.firstName} ${skater.lastName}`}</h3>
-                <div className={styles.actionContainer}>
-                  <button className={styles.actionBtn}>
-                    <LiaIdCard />
-                  </button>
-                  <button className={styles.actionBtn}>
-                    <LiaUserEditSolid />
-                  </button>
-                  <button className={styles.actionBtn}>
-                    <LiaTrashAlt />
-                  </button>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className={styles.skatersContainer}>
+          {skaters.length > 0
+            ? (skaters.map((skater) => (
+              <figure className={styles.figure} key={skater._id}>
+                <header className={styles.header}>
+                  <h3
+                    className={styles.figureHeading}
+                  >{`${skater.firstName} ${skater.lastName}`}</h3>
+                  <div className={styles.actionContainer}>
+                    <button className={styles.actionBtn}>
+                      <LiaIdCard />
+                    </button>
+                    <button className={styles.actionBtn} onClick={() => onEditSkater(skater)}>
+                      <LiaUserEditSolid />
+                    </button>
+                    <button className={styles.actionBtn} onClick={() => onDeleteSkater(skater)}>
+                      <LiaTrashAlt />
+                    </button>
+                  </div>
+                </header>
+                <div className={styles.content}>
+                  <p className={styles.element}>
+                    <span className={styles.elSpan}>{lang.gender}: </span>
+                    {skater.gender}
+                  </p>
+                  <p className={styles.element}>
+                    <span className={styles.elSpan}>{lang.level}: </span>
+                    {skater.level}
+                  </p>
+                  <p className={styles.element}>
+                    <span className={styles.elSpan}>{lang.age}: </span>
+                    {skater.age}
+                  </p>
                 </div>
-              </header>
-              <div className={styles.content}>
-                <p className={styles.element}>
-                  <span className={styles.elSpan}>{lang.gender}: </span>
-                  {skater.gender}
-                </p>
-                <p className={styles.element}>
-                  <span className={styles.elSpan}>{lang.level}: </span>
-                  {skater.level}
-                </p>
-                <p className={styles.element}>
-                  <span className={styles.elSpan}>{lang.age}: </span>
-                  {skater.age}
-                </p>
-              </div>
-            </figure>
-          ))}
-      </div>
+              </figure>
+            ))
+            ) : (
+              <h2 className={styles.headingNoSkaters}>{lang.noAddedSkaters}</h2>
+            )}
+        </div>
+      )}
 
       <div className={styles.addSkaterBtnContainer}>
-        <button className={styles.addBtn}>{lang.addSkater}</button>
+        <button className={styles.addBtn} onClick={toggleAddSkaterModalHandler}>{lang.addSkater}</button>
       </div>
 
       <section className={styles.description}>
@@ -106,6 +105,11 @@ function SkatersList() {
           в списъка.
         </p>
       </section>
+
+      {isShownAddSkaterModal && <AddSkater onClose={toggleAddSkaterModalHandler} />}
+      {isShownEditSkaterModal && <EditSkater skaterData={selectedSkaterData} onClose={toggleEditSkaterModalHandler} />}
+      {isShownDeleteSkaterModal && <DeleteSkater skaterData={selectedSkaterData} onClose={toggleDeleteSkaterModalHandler} />}
+
     </div>
   );
 }
