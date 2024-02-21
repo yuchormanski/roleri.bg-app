8f921efb2124099b527424951d679059d5702d76
Todor Yadkov <toni_12@abv.bg>
2024-02-21 19:17:11 +0200

    Create useEditSkaterQuery

---
+++ client/src/features/users/useEditSkaterQuery.js
@@ -0,0 +1,28 @@
+import toast from "react-hot-toast";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { SERVER_ENDPOINTS } from "../../services/environment.js";
+
+import { put } from "../../api/api.js";
+
+function useEditSkaterQuery() {
+    const queryClient = useQueryClient();
+
+    const editSkaterMutation = useMutation({
+        enabled: false,
+        mutationFn: (skaterData) => put(SERVER_ENDPOINTS.EDIT_SKATER, skaterData),
+        onSuccess: (skaterData) => {
+            toast.success("Skater updated successfully!");
+            // Save the skater data to the cache
+            queryClient.setQueryData(["skaters"], oldData => oldData.map(skater => skater._id === skaterData._id ? skaterData : skater));
+
+        },
+        onError: (error) => {
+            toast.error(error.message);
+        },
+    });
+
+    return { editSkaterMutation };
+}
+
+export { useEditSkaterQuery };
