16d4529d3c1e2046ebe6a1f05b8c38de5c44a19b
Todor Yadkov <toni_12@abv.bg>
2024-02-21 19:23:34 +0200

    Create skate service

---
+++ server/services/skaterService.js
@@ -0,0 +1,19 @@
+import { SkaterModel } from '../models/SkaterModel.js';
+
+const getAllSkaters = async () => SkaterModel.find();
+
+const getSkaterById = async (skaterId) => SkaterModel.findById(skaterId);
+
+const addSkater = (userParentId, skaterData) => SkaterModel.create({ ...skaterData, owner: userParentId });
+
+const updateSkater = (skaterId, skaterData) => SkaterModel.findByIdAndUpdate(skaterId, skaterData, { runValidators: true, new: true });
+
+const deleteSkater = async (skaterId) => SkaterModel.findByIdAndDelete(skaterId);
+
+export {
+    getAllSkaters,
+    getSkaterById,
+    addSkater,
+    updateSkater,
+    deleteSkater,
+}
