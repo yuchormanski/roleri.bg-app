203202071b5d76ee6809acaa3b9bd2daa41ba5cb
Todor Yadkov <toni_12@abv.bg>
2024-02-21 19:23:49 +0200

    Add new validation schema

--- server/util/validationSchemes.js
+++ server/util/validationSchemes.js
@@ -2,6 +2,7 @@ import joi from "joi";
 
 import { userRole } from "../environments/constants.js";
 
+// User Password messages
 const commonPasswordMessages = {
   "string.base": "Password must be a string",
   "string.pattern.base":
@@ -11,6 +12,7 @@ const commonPasswordMessages = {
   "string.max": "Password cannot be longer than {#limit} characters",
 };
 
+// User Password validation
 const passwordSchema = joi
   .string()
   .regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,20}$/)
@@ -18,6 +20,7 @@ const passwordSchema = joi
   .trim()
   .messages(commonPasswordMessages);
 
+// User register validation
 const validateRegisterSchema = joi.object({
   firstName: joi.string().required().trim().max(100),
   lastName: joi.string().required().trim().max(100),
@@ -38,6 +41,7 @@ const validateRegisterSchema = joi.object({
   password: passwordSchema,
 });
 
+// User update validation
 const updateUserSchema = joi.object({
   firstName: joi.string().required().trim().max(100),
   lastName: joi.string().required().trim().max(100),
@@ -57,14 +61,46 @@ const updateUserSchema = joi.object({
     .lowercase(),
 });
 
+// User login validation
 const validateLoginSchema = joi.object({
   email: joi.string().required().trim().email().lowercase(),
   password: passwordSchema,
 });
 
+// User reset password validation
 const validateResetPasswordSchema = joi.object({
   password: passwordSchema,
   resetToken: joi.string().required().trim(),
+});
+
+// Lesson validation
+const lessonCreateSchema = joi.object({
+  imageUrl: joi.string().required().regex(/^https?:\/\/.+/),
+  title: joi.string().required(),
+  titleInfo: joi.string().required(),
+  age: joi.string().required(),
+  skills: joi.string().required(),
+  participants: joi.string().required(),
+  type: joi.string().required(),
+  count: joi.string().required(),
+  location: joi.string().required(),
+  price: joi.string().required(),
+  geoLocation: joi.object({
+    lat: joi.string().allow(null).default(null),
+    lon: joi.string().allow(null).default(null)
+  }),
+  description: joi.string().required(),
+});
+
+// Skater validation
+const skaterCreateSchema = joi.object({
+  firstName: joi.string().required().max(20),
+  lastName: joi.string().required().max(20),
+  age: joi.number().required(),
+  gender: joi.string().required(),
+  skatesSize: joi.number().required().min(0),
+  protection: joi.string().required(),
+  level: joi.string().required(),
 });
 
 export {
@@ -72,4 +108,6 @@ export {
   validateLoginSchema,
   validateResetPasswordSchema,
   updateUserSchema,
+  lessonCreateSchema,
+  skaterCreateSchema,
 };
