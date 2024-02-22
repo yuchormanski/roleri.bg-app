7ab4aa38521dee919075a1e6ac5a2963f4a2cb91
Todor Yadkov <toni_12@abv.bg>
2024-02-23 01:03:43 +0200

    test

--- server/services/userService.js
+++ server/services/userService.js
@@ -205,7 +205,7 @@ function cookieOptions() {
   return process.env.NODE_ENV === "production"
     ? {
       httpOnly: true,
-      sameSite: "lax",
+      sameSite: "none",
       secure: true,
       maxAge: calculateExpirePeriodCookieInDay(),
     }
