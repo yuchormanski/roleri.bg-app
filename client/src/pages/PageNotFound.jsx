1f4bfec684fb355cf262661833c897d60b565e49
yuchormanski <yuchormanski@gmail.com>
2024-03-03 11:06:40 +0200

    changed error boundary

--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -1,14 +1,26 @@
+import styles from "./PageNotFound.module.css";
 import { useMoveBack } from "../hooks/useMoveBack";
 
 function PageNotFound() {
   const { moveBack } = useMoveBack();
 
   return (
+    // <>
+    //   <h3>The page you are looking for could not be found 😢</h3>
+    //   <button onClick={moveBack} size="large">
+    //     &larr; Go back
+    //   </button>
+    // </>
+
     <>
-      <h3>The page you are looking for could not be found 😢</h3>
-      <button onClick={moveBack} size="large">
-        &larr; Go back
-      </button>
+      <div className={styles.page}>
+        <h2 className={styles.heading}>Something went wrong!</h2>
+        <p className={styles.friendlyMessage}>Please, try again later</p>
+        {/* <pre className={styles.errorMessage}>{error.message}</pre> */}
+        <a href="/" className={styles.link}>
+          Return to Home
+        </a>
+      </div>
     </>
   );
 }
