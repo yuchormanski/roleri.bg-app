6f553033d7e0ede851886cd0a63baf6a5d937657
yuchormanski <yuchormanski@gmail.com>
2024-03-04 09:40:58 +0200

    spinner image is changed according to theme color

--- client/src/ui/elements/spinner/Spinner.jsx
+++ client/src/ui/elements/spinner/Spinner.jsx
@@ -1,9 +1,12 @@
+import { useTheme } from "../../../context/DarkMode.jsx";
 import styles from "./Spinner.module.css";
 
 function Spinner() {
+  const { isDark } = useTheme();
+  const src = isDark ? "/wheel2.png" : "/apple-touch-icon.png";
   return (
     <div className={styles.spinnerBlock}>
-      <img src="/wheel2.png" alt="Loading image" className={styles.rotate} />
+      <img src={src} alt="Loading image" className={styles.rotate} />
     </div>
   );
 }
