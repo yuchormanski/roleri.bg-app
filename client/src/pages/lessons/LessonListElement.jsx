c1c70b176cb382f32cb00d6b31d0e6510ddc1183
yuchormanski <yuchormanski@gmail.com>
2024-03-07 08:38:20 +0200

    styles on lesson element

--- client/src/pages/lessons/LessonListElement.jsx
+++ client/src/pages/lessons/LessonListElement.jsx
@@ -9,24 +9,42 @@ function LessonListElement({ lesson }) {
   const { lang } = useLanguage();
   const { translatePhrase: translate } = useTranslate();
 
+  function reveal() {
+    let reveals = document.querySelectorAll(".card");
+
+    for (let i = 0; i < reveals.length; i++) {
+      let windowHeight = window.innerHeight;
+      let elementTop = reveals[i].getBoundingClientRect().top;
+      let elementVisible = 10;
+
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+  window.addEventListener("scroll", reveal);
+
   return (
-    <Link to={`/lesson/${lesson._id}`}>
-      <figure className={styles.card}>
-        <div className={styles.imgBox}>
-          <img src={lesson.imageUrl} alt="" />
+    <figure className={styles.card}>
+      <div className={styles.imgBox}>
+        <img src={lesson.imageUrl} alt="" />
+      </div>
+      <div className={styles.textBox}>
+        <span className={styles.title}>{translate(lesson.title)}</span>
+      </div>
+      <div className={styles.main}>
+        <div className={styles.info}>
+          <p>{translate(lesson.skills)}</p>
+          <p>{translate(lesson.location)}</p>
         </div>
-        <div className={styles.textBox}>
-          <span>{translate(lesson.title)}</span>
-        </div>
-        <div className={styles.main}>
-          <div className={styles.info}>
-            <p>{translate(lesson.skills)}</p>
-            <p>{translate(lesson.location)}</p>
-          </div>
+        <Link to={`/lesson/${lesson._id}`}>
           <p className={styles.button}>{lang.learnMore}</p>
-        </div>
-      </figure>
-    </Link>
+        </Link>
+      </div>
+    </figure>
   );
 }
 
