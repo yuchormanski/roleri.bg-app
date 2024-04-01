ccdb506dbe3c3dee8696df776f9751cbf5cd88ad
yuchormanski <yuchormanski@gmail.com>
2024-04-01 08:06:05 +0300

    fixed reg form

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -19,10 +19,21 @@ import Button from "../../ui/elements/button/Button.jsx";
 import Spinner from "../../ui/elements/spinner/Spinner.jsx";
 import { Link } from "react-router-dom";
 
+const initialFieldsValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  phone: "",
+  password: "",
+  repass: "",
+};
+
 function Register({ onOut = null, onClose, authToggle }) {
   const { lang } = useLanguage();
   const [isNotForgotten, setIsNotForgotten] = useState(false);
   const [terms, setTerms] = useState(false);
+  const [fieldValues, setFieldValues] = useState(initialFieldsValues);
+
   const { register, handleSubmit, reset, getValues, formState } = useForm();
 
   const { registerMutation } = useAuthQueries();
@@ -74,6 +85,12 @@ function Register({ onOut = null, onClose, authToggle }) {
     } else setTerms(false);
   }
 
+  function valueHandler(e) {
+    const valueName = e.target.name;
+    const value = e.target.value;
+    setFieldValues({ ...fieldValues, [valueName]: value });
+  }
+
   return (
     <>
       {registerMutation.isPending && <Spinner />}
@@ -89,105 +106,157 @@ function Register({ onOut = null, onClose, authToggle }) {
           onSubmit={handleSubmit(onFormSubmit, onErrorSubmit)}
           className={styles.form}
         >
-          <input
-            className={styles.input}
-            type="text"
-            id="firstName"
-            {...register("firstName", {
-              required: "First name is required",
-              maxLength: {
-                value: 20,
-                message: "First name can't be more than 20 characters long!",
-              },
-            })}
-            placeholder={"First name"}
-            autoComplete="given-name"
-          />
-          <input
-            className={styles.input}
-            type="text"
-            id="lastName"
-            {...register("lastName", {
-              required: "Last name is required",
-              maxLength: {
-                value: 20,
-                message: "Last name can't be more than 20 characters long!",
-              },
-            })}
-            placeholder={"Last name"}
-            autoComplete="family-name"
-          />
-          <input
-            className={styles.input}
-            type="text"
-            id="email"
-            {...register("email", {
-              required: "Email is required",
-              pattern: {
-                value: EMAIL_REGEX,
-                message: "Invalid email address",
-              },
-              maxLength: {
-                value: 30,
-                message: "Email can't be more than 30 characters long!",
-              },
-            })}
-            placeholder={"Email"}
-            autoComplete="email"
-          />
+          <div className={styles.element}>
+            <input
+              className={styles.textInput}
+              type="text"
+              id="firstName"
+              name={"firstName"}
+              {...register("firstName", {
+                required: "First name is required",
+                maxLength: {
+                  value: 20,
+                  message: "First name can't be more than 20 characters long!",
+                },
+              })}
+              onBlur={valueHandler}
+              autoComplete="given-name"
+            />
+            <label
+              htmlFor={"firstName"}
+              className={`${styles.label} ${
+                fieldValues.firstName ? styles.filled : null
+              }`}
+            >
+              {lang.firstName}
+            </label>
+          </div>
+
+          <div className={styles.element}>
+            <input
+              className={styles.textInput}
+              type="text"
+              id="lastName"
+              name={"lastName"}
+              {...register("lastName", {
+                required: "Last name is required",
+                maxLength: {
+                  value: 20,
+                  message: "Last name can't be more than 20 characters long!",
+                },
+              })}
+              onBlur={valueHandler}
+              autoComplete="family-name"
+            />
+            <label
+              htmlFor={"lastName"}
+              className={`${styles.label} ${
+                fieldValues.lastName ? styles.filled : null
+              }`}
+            >
+              {lang.lastName}
+            </label>
+          </div>
+
+          <div className={styles.element}>
+            <input
+              className={styles.textInput}
+              name={"email"}
+              type="email"
+              id="email"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: EMAIL_REGEX,
+                  message: "Invalid email address",
+                },
+                maxLength: {
+                  value: 30,
+                  message: "Email can't be more than 30 characters long!",
+                },
+              })}
+              onBlur={valueHandler}
+              autoComplete="email"
+            />
+            <label
+              htmlFor={"email"}
+              className={`${styles.label} ${
+                fieldValues.email ? styles.filled : null
+              }`}
+            >
+              {lang.email}
+            </label>
+          </div>
 
           <div className={styles.passContainer}>
-            <input
-              className={styles.input}
-              type={visible ? "text" : "password"}
-              id="password"
-              {...register(
-                "password",
-                !isNotForgotten
-                  ? {
-                      required: "Password is required",
-                      minLength: {
-                        value: 3,
-                        message:
-                          "The password should be at least 3 characters long ",
-                      },
-                      pattern: {
-                        value: PASS_REGEX,
-                        message:
-                          "Password must contain at least one lowercase letter, one uppercase letter, and one digit",
-                      },
-                    }
-                  : null
-              )}
-              placeholder={"Password"}
-            />
+            <div className={styles.element}>
+              <input
+                className={styles.textInput}
+                name={"password"}
+                type={visible ? "text" : "password"}
+                id="password"
+                {...register(
+                  "password",
+                  !isNotForgotten
+                    ? {
+                        required: "Password is required",
+                        minLength: {
+                          value: 3,
+                          message:
+                            "The password should be at least 3 characters long ",
+                        },
+                        pattern: {
+                          value: PASS_REGEX,
+                          message:
+                            "Password must contain at least one lowercase letter, one uppercase letter, and one digit",
+                        },
+                      }
+                    : null
+                )}
+                onBlur={valueHandler}
+              />
+              <label
+                htmlFor={"password"}
+                className={`${styles.label} ${
+                  fieldValues.password ? styles.filled : null
+                }`}
+              >
+                {lang.password}
+              </label>
+            </div>
+
             <div className={styles.passEyeBtn} onClick={eyeHandler}>
               {visible ? <GoEyeClosed /> : <GoEye />}
             </div>
           </div>
 
-          {/* <div className={styles.passContainer}> */}
-          <input
-            className={styles.input}
-            type={visible ? "text" : "password"}
-            id="repass"
-            {...register("repass", {
-              required: "Repeat password is required",
-              validate: (value) =>
-                value === getValues().password || "Passwords don't match",
-              pattern: {
-                value: PASS_REGEX,
-                message:
-                  "Password must contain at least one lowercase letter, one uppercase letter, and one digitt",
-              },
-            })}
-            placeholder={"Repeat password"}
-            autoComplete="new-password"
-          />
-          {/* <div className={styles.passEyeBtn} onClick={eyeHandler}>
-              {visible ? <GoEyeClosed /> : <GoEye />}
-            </div> */}
-          {/* </div> */}
+          <div className={styles.element}>
+            <input
+              className={styles.textInput}
+              name={"repass"}
+              type={visible ? "text" : "password"}
+              id="repass"
+              {...register("repass", {
+                required: "Repeat password is required",
+                validate: (value) =>
+                  value === getValues().password || "Passwords don't match",
+                pattern: {
+                  value: PASS_REGEX,
+                  message:
+                    "Password must contain at least one lowercase letter, one uppercase letter, and one digitt",
+                },
+              })}
+              onBlur={valueHandler}
+            />
+            <label
+              htmlFor={"repass"}
+              className={`${styles.label} ${
+                fieldValues.repass ? styles.filled : null
+              }`}
+            >
+              {lang.repass}
+            </label>
+          </div>
 
           <PhoneInput
             name={"phone"}
@@ -196,7 +265,7 @@ function Register({ onOut = null, onClose, authToggle }) {
             onChange={(phone) => setPhone(phone)}
             inputStyle={{
               border: "1px solid var(--input-border)",
-              backgroundColor: "var(--color-input)",
+              backgroundColor: "var(--color-header)",
               borderRadius: "3px",
               fontSize: "1.6rem",
               padding: "0 2px",
@@ -208,13 +277,13 @@ function Register({ onOut = null, onClose, authToggle }) {
             buttonStyle={true}
             style={{
               "--react-international-phone-dropdown-item-background-color":
-                "var(--color-input)",
+                "var(--color-header)",
               "--react-international-phone-country-selector-background-color":
-                "var(--color-input)",
+                "var(--color-header)",
               "--react-international-phone-country-selector-background-color-hover":
-                "var(--color-input)",
+                "var(--color-header)",
               "--react-international-phone-country-selector-border-color":
-                "var(--input-border)",
+                "none",
               "--react-international-phone-country-selector-arrow-color":
                 "var(--color-main)",
               "--react-international-phone-dropdown-item-text-color":
